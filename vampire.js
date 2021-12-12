2e0da64daca225b573b0055b452bceef52ebc6d2
Eric Moolenbeek <emoolenbeek@gmail.com>
2021-12-12 22:45:35 +0000

    initial commit

--- vampire.js
+++ vampire.js
@@ -10,22 +10,47 @@ class Vampire {
 
   // Adds the vampire as an offspring of this vampire
   addOffspring(vampire) {
-
+    this.offspring.push(vampire);
+    vampire.creator = this;
   }
 
   // Returns the total number of vampires created by that vampire
   get numberOfOffspring() {
-
+    return this.offspring.length;
   }
 
   // Returns the number of vampires away from the original vampire this vampire is
   get numberOfVampiresFromOriginal() {
-
+    let current = this;
+    let count = 0;
+    while (current.creator) {
+      current = current.creator;
+      count++;
+    }
+    return count;      
   }
 
   // Returns true if this vampire is more senior than the other vampire. (Who is closer to the original vampire)
   isMoreSeniorThan(vampire) {
+    let vamp1 = vampire;
+    let vamp2 = this;
+    let count1 = 0;
+    let count2 = 0;
 
+    while (vamp1.creator) {
+      vamp1 = vamp1.creator;
+      count1++;
+    }
+
+    while (vamp2.creator) {
+      vamp2 = vamp2.creator;
+      count2++;
+    }
+
+    if (count1 > count2) {
+      return true;
+    }
+    return false;
   }
 
   /** Stretch **/
